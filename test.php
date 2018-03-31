39f47884ab198518ed7af1a9d8257ff62ce00b32
The Creator <nb3slayer@gmail.com>
2018-03-31 11:47:05 +0200

    net

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -7,12 +13,12 @@
         <?php
         echo
         "<form action='' method='post'>
-        <input type='submit' name='command' value='ipconfig' />
+        <input type='submit' name='command' value='Network Properties' />
         </form>";
 
         if(isset($_POST['command']))
         {
-            $cmd= shell_exec('ipconfig /all');
+            $cmd= shell_exec('ifconfig -a');
             echo "<pre>$cmd</pre>";
         }
         ?>
